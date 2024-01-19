9f10b413862877e0e176746b1283d6a439e1915a
Bilge Kagan Celik <bilgekagancelik@outlook.com>
2024-01-19 07:03:17 -0500

    hour component up-to-date

--- src/components/Hour/index.jsx
+++ src/components/Hour/index.jsx
@@ -1,13 +1,27 @@
 import './index.css';
 
 function Hour() {
+    const months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+    const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    const date = new Date();
+
+    let month = months[date.getMonth()];
+    let day = days[date.getDay()];
+
+    let year = date.getFullYear();
+    let hour = date.getHours();
+    let minute = date.getMinutes();
+
     return (
         <div>
             <div className='hour-container'>
-                <p className='hour'>15.30</p>
-                <div className='utc-container'>
-                    <p className='utc'>UTC+3</p>
+                <div className='year-container'>
+                    <p>{day},</p>
+                    <p>{month}</p>
+                    <p>{year}</p>
+
                 </div>
+                <p className='hour'>{hour}:{minute}</p>
             </div>
         </div>
     )
